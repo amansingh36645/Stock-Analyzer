15c997cabf4f59100dd7533b40fd95ff08eb58d6
Aman Singh <amansingh36645@gmail.com>
2026-07-09 20:07:11 +0530

    Progress in Home Page UI design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -94,6 +94,54 @@ const Home = () => {
           </div>
         </div>
       </div>
+      {/* home page second  */}
+      <div className="">
+        {/* vertical div 1 */}
+        <div className="text-center">
+          <h1 className="text-3xl font-bold text-gray-600">
+            Building Awesome Investing & Trading Platforms
+          </h1>
+        </div>
+        {/* vertical div 2 */}
+        <div className="flex items-center justify-around ">
+          <div className="flex flex-col w-1/2 gap-5">
+            <h1 className="text-4xl font-bold">Dhan App</h1>
+            <p className="w-3/4 text-gray-600 text-lg ">
+              A modern stock analysis platform built to deliver real-time market
+              insights, company information, interactive charts, and financial
+              news—helping users make informed investment decisions with
+              confidence.
+            </p>
+            <button className=" bg-amber-400 w-30 text-center text-xl rounded-lg">
+              Explore
+            </button>
+          </div>
+          <div className="">
+            <img width={200} src="/iphone-ipad.png" alt="" />
+          </div>
+        </div>
+      </div>
+      {/* home page 3rd scene */}
+      <div>
+        <div>
+          <div>
+            <h1>Trade on Trading Terminal with DEXT</h1>
+          </div>
+          <div>
+            <div>
+              <p>30+ Widgets</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>40+ Keyboard Shortcuts</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Trade smoothly</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
